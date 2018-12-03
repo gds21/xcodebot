d913b083ec25c8c96b738feac6a3777a95b9513b
gds21 <gds721@gmail.com>
2018-12-03 10:09:46 +0800

    first commit

---
+++ xcodebot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xcodebot
+//
+//  Created by Xue Xin Tsai on 2018/12/3.
+//  Copyright © 2018 Xue Xin Tsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
